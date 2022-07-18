be44ca9e36744739536d0f931a668431c3fa3365
Kaif Shaikh <kaifs0351@gmail.com>
2022-07-18 14:00:22 +0530

    build-18Jul

---
+++ build/precache-manifest.8563dc3a2fbfe424b548f14647ec6cd0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cddbd196576ee718fa45651c9d7e7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97a83ac8dbb5e574a17a",
+    "url": "/static/css/2.0520b0af.chunk.css"
+  },
+  {
+    "revision": "56c4aa0196edcd2ec0d7",
+    "url": "/static/css/main.6fe5df36.chunk.css"
+  },
+  {
+    "revision": "97a83ac8dbb5e574a17a",
+    "url": "/static/js/2.5f1e0a21.chunk.js"
+  },
+  {
+    "revision": "1428bb987169061ecc361902eb390073",
+    "url": "/static/js/2.5f1e0a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c4aa0196edcd2ec0d7",
+    "url": "/static/js/main.da0d3e46.chunk.js"
+  },
+  {
+    "revision": "6c6158fdec520101d06e",
+    "url": "/static/js/runtime-main.fe128b73.js"
+  },
+  {
+    "revision": "a6f06eee7f5ce57a78144ccd935b4730",
+    "url": "/static/media/DYPIU_22June2021_adhoc-banner.a6f06eee.png"
+  },
+  {
+    "revision": "b579e8cefc0808ec1be635cc1d9b571f",
+    "url": "/static/media/home-banner-03.b579e8ce.jpeg"
+  },
+  {
+    "revision": "e5ce76edf89ce299c934fe68b007f288",
+    "url": "/static/media/home-banner-04.e5ce76ed.jpeg"
+  }
+]);
