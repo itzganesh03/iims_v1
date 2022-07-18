8c4a8a3c946363ec8256bc9b331628296d6bb1ef
Kaif Shaikh <kaifs0351@gmail.com>
2022-07-18 12:56:14 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,42 +6,43 @@ import Carousel from "react-bootstrap/Carousel";
 const Home = () => {
   return (
     <>
-      <div>
-        <div className="container-flex">
-          <Carousel>
-            <Carousel.Item style={{ height: "300px" }}>
-              <img
-                style={{ height: "300px" }}
-                className="d-block w-100"
-                src={web}
-              />
-              <Carousel.Caption>
-                <h3>First Demo </h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item style={{ height: "300px" }}>
-              <img
-                style={{ height: "300px" }}
-                className="d-block w-100"
-                src={web1}
-              />
-              <Carousel.Caption>
-                <h3>Second Demo</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item style={{ height: "300px" }}>
-              <img
-                style={{ height: "300px" }}
-                className="d-block w-100"
-                src={web2}
-              />
-              <Carousel.Caption>
-                <h3>Third Demo</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+      <div className="blue-bar"></div>
+        <div>
+          <div className="container-flex">
+            <Carousel>
+              <Carousel.Item style={{ height: "300px" }}>
+                <img
+                  style={{ height: "300px" }}
+                  className="d-block w-100"
+                  src={web}
+                />
+                <Carousel.Caption>
+                  <h3>First Demo </h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item style={{ height: "300px" }}>
+                <img
+                  style={{ height: "300px" }}
+                  className="d-block w-100"
+                  src={web1}
+                />
+                <Carousel.Caption>
+                  <h3>Second Demo</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item style={{ height: "300px" }}>
+                <img
+                  style={{ height: "300px" }}
+                  className="d-block w-100"
+                  src={web2}
+                />
+                <Carousel.Caption>
+                  <h3>Third Demo</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
         </div>
-      </div>
     </>
   );
 };
