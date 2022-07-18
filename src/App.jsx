1d9bb89487d0e61a9a32369551f44440bed544e8
Kaif Shaikh <67305606+kaifshaikhhhh@users.noreply.github.com>
2022-07-18 20:00:04 +0530

    everyone contribute on the temp

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from "./Home";
+import Classroom from "./Classroom";
+import Exam from "./Exam";
+import Calender from "./Calender";
+import Faculty from "./Faculty";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import LostandFound from "./LostandFound";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+
+//Start Preloader for Welcome Screen
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 3000);
+  }
+  //End Preloader for Welcome Screen
+  return (
+    !loading && (
+    <>
+    
+      <Navbar />
+      <Switch>
+      <Route exact path="/" component={Home} />
+        <Route exact path="/calender" component={Calender} />
+        <Route exact path="/classroom" component={Classroom} />
+        <Route exact path="/faculty" component={Faculty} />
+        <Route exact path="/exam" component={Exam} />
+        <Route exact path="/lostandfound" component={LostandFound} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer />
+    </>
+    )
+  );
+};
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import web from "../src/images/DYPIU_22June2021_adhoc-banner.png";
+import web1 from "../src/images/home-banner-04.jpeg";
+import web2 from "../src/images/home-banner-03.jpeg";
+import Carousel from "react-bootstrap/Carousel";
+const Home = () => {
+  return (
+    <>
+        <div>
+          <div className="container-flex">
+            <Carousel>
+              <Carousel.Item style={{ height: "531px" }}>
+                <img
+                  style={{ height: "531px" }}
+                  className="d-block w-100"
+                  src={web}
+                />
+                <Carousel.Caption>
+                  <h3>First Demo </h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item style={{ height: "531px" }}>
+                <img
+                  style={{ height: "531px" }}
+                  className="d-block w-100"
+                  src={web1}
+                />
+                <Carousel.Caption>
+                  <h3>Second Demo</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item style={{ height: "531px" }}>
+                <img
+                  style={{ height: "531px" }}
+                  className="d-block w-100"
+                  src={web2}
+                />
+                <Carousel.Caption>
+                  <h3>Third Demo</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default Home;
