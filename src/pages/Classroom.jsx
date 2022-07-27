b473bec8fa3d8171a0574a109ec800f41d9c30a8
itzganesh <modiganesh83@gmail.com>
2022-07-27 23:40:20 +0530

    Mrg_Temp_Branch

--- src/pages/Classroom.jsx
+++ src/pages/Classroom.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useState } from "react";
+import ButtonTabs from '../components/classroomTabs/classroomTabs.component';
+import Class from "../components/class/class.component";
 
 const Classroom = () => {
+
+  const [tab, setTab] = useState("3rdFloor");
+
+  const handleClick = (btnName) => {
+    setTab(btnName);
+  };
+
   return (
-    <div>Classroom</div>
+    <div className="Classroom">
+      {/* Button Tabs */}
+      <ButtonTabs onClickHandler={handleClick} currentTab={tab} />
+
+      {/* Classes */}
+      <Class started={true} />
+      <Class started={false} />
+    </div>
   )
 }
 
