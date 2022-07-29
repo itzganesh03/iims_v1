1dec8bd0fbc10465f567cda940dbdcf5ab94df8a
itzganesh <modiganesh83@gmail.com>
2022-07-29 23:20:58 +0530

    Update_Static_Data_Api_Folder

---
+++ src/Data/facultyRequestData.js
@@ -0,0 +1,76 @@
+const data = [
+    {
+      request: "pending",
+      name: "ABC",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "B.Tech BioEng",
+      note: ""
+    },
+    {
+      request: "pending",
+      name: "CIJK",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "B.Tech CSE",
+      note: ""
+    },
+    {
+      request: "pending",
+      name: "KLM",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "MBA",
+      note: ""
+    },
+    {
+      request: "pending",
+      name: "GHI",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "MBA",
+      note: ""
+    },
+    {
+      request: "accepted",
+      name: "CDE",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "MBA",
+      note: ""
+    },
+    {
+      request: "accepted",
+      name: "EFG",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "MBA",
+      note: ""
+    },
+    {
+      request: "accepted",
+      name: "MNO",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "MBA",
+      note: ""
+    },
+    {
+      request: "accepted",
+      name: "OPQ",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "MBA",
+      note: ""
+    },
+    {
+      request: "accepted",
+      name: "QRS",
+      prn: 12345678908,
+      year: "1st Year",
+      course: "MBA",
+      note: ""
+    }
+  ];
+  export default data;
+  
